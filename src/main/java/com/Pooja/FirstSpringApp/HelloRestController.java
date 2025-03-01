f57d5a4509af491f4b94bf43f5a91530889dc8b8
pooja <pooja.gla_cs21@gla.ac.in>
2025-03-01 14:44:57 +0530

    https methods

---
+++ src/main/java/com/Pooja/FirstSpringApp/HelloRestController.java
@@ -0,0 +1,29 @@
+
+package com.Pooja.FirstSpringApp;
+
+import com.Pooja.FirstSpringApp.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @GetMapping()
+    public String sayHello() {
+        return "Hello From Pooja - GET REGUEST ";
+    }
+    @PutMapping()
+        public String putHello(){
+            return "Hello from Pooja - PUT REQUEST ";
+
+    }
+    @PostMapping()
+    public String postHello(){
+        return "Hello from Pooja -POST REQUEST";
+    }
+    @DeleteMapping()
+    public String deleteHello(){
+        return "Hello from Pooja-DELETE REQUEST";
+    }
+
+}
